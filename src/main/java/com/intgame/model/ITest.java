03e4cedeb46ec10e3ea37d0b96be65ddf2d719d6
vviital <vviital@gmail.com>
2015-10-31 14:14:52 +0300

    Change

--- src/main/java/com/intgame/model/ITest.java
+++ src/main/java/com/intgame/model/ITest.java
@@ -3,5 +3,6 @@ package com.intgame.model;
 public class ITest {
     public static void main(String[] args) {
         System.out.println("Hello everybody from Ira");
+        System.out.println();
     }
 }
